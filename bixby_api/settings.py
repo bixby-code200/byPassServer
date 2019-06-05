be0b564c406d374a37118565321faab703d918e6
unknown <onelife0985@gmail.com>
2019-06-06 01:08:35 +0900

    for AWS, basic setup

--- bixby_api/settings.py
+++ bixby_api/settings.py
@@ -30,8 +30,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework',
-    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
